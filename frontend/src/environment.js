e5f4f77ddf67e792136ef24074502f0c4dc0ec5d
GAUTAM AGRAWAL <gautamagrawal@GAUTAMs-MacBook-Air.local>
2026-01-21 23:04:25 +0530

    fix: use HTTPS API Gateway URL for backend

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,7 +1,7 @@
 const IS_PROD = true;
 
 const server = IS_PROD
-  ? "http://3.110.127.147"
+  ? "https://e71xuvrhq4.execute-api.ap-south-1.amazonaws.com"
   : "http://localhost:8000";
 
 export default server;
